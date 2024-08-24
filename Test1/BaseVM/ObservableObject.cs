ba1fc66aaae18a3aabd921ba1f84f4e1e912f779
ArunKumarLingutla <arunkumarlingutla94177@gmail.com>
2024-08-24 10:22:12 +0530

    Add project files.

---
+++ Test1/BaseVM/ObservableObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1.BaseVM
+{
+    class ObservableObject:INotifyPropertyChanged
+    {
+        private bool _isLoading;
+
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set
+            {
+                if (_isLoading != value)
+                {
+                    _isLoading = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void SetLoading(bool isLoading)
+        {
+            IsLoading = isLoading;
+        }
+    }
+}
